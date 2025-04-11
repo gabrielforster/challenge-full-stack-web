4ab310ad4fc89167ca719f027c0d2bc731180b6e
Gabriel Rocha <rochafrgabriel@gmail.com>
2025-04-10 23:09:26 -0300

    feat: init frontend app

---
+++ app/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from "vue"
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+const increment = () => {
+  count.value++
+}
+</script>
+
+<template>
+  <h1 class="text-xl">Hi from Vue app</h1>
+  <button @click="increment">Count is: {{ count }}</button>
+</template>
