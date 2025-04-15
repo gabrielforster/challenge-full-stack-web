2aaf77596b9efb0b3b0ef364cf9f7130e373e0d0
Gabriel Rocha <rochafrgabriel@gmail.com>
2025-04-15 20:07:06 -0300

    feat: frontend user listing and routes definitions

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue"
+import { createRouter, createWebHistory } from "vue-router"
 import "./style.css"
 import App from "./App.vue"
 
@@ -7,11 +8,24 @@ import { createVuetify } from "vuetify"
 import * as components from "vuetify/components"
 import * as directives from "vuetify/directives"
 
+import Home from "./routes/home.vue"
+import Students from "./routes/students.vue"
+
 const vuetify = createVuetify({
   components,
   directives,
 })
 
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", component: Home },
+    { path: "/alunos", component: Students },
+    { path: "/estudantes", component: Students },
+  ]
+})
+
 createApp(App)
   .use(vuetify)
+  .use(router)
   .mount("#app")
